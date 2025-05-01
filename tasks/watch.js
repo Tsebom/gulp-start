392380c02bf6f92aa2bc1599d4edacea75bf9af7
Tsebom <Tsebom@yandex.ru>
2025-05-02 00:48:48 +0400

    delete images from build/images and app/images directories

--- tasks/watch.js
+++ tasks/watch.js
@@ -27,15 +27,27 @@ export const watching = () => {
 		})
 	});
 
+	const wh_rastr = watch('app/images/**/*.+(png|jpg|jpeg|gif|svg|ico)', parallel(rastr));
+
+	wh_rastr.on('unlink', function(file) {
+		let name = path.basename(file).split('.')[0];
+
+		findRemoveSync('build/images/', {
+			prefix : name
+		})
+
+		findRemoveSync('app/images/', {
+			prefix : name
+		})
+	});
+
 	watch('app/**/*.html', parallel(html));
 	watch('app/**/*.php', parallel(php));
 	watch('app/**/*.scss', parallel(styles));
 	watch('app/**/*.js', parallel(scripts));
 	watch('app/**/*.json', parallel(html));
-	watch('app/images/**/*.+(png|jpg|jpeg|gif|svg|ico)', parallel(rastr));
+	watch('app/svg/sprite/**/*.svg', series(svg_sprite, rastr));
 	watch('build/images/**/*.+(png|jpg|jpeg)', parallel(webp));
 	watch('app/svg/css/**/*.svg', series(svg_css, styles));
-	watch('app/svg/sprite/**/*.svg', series(svg_sprite, rastr));
-	
 	watch('build/fonts/**/*.woff2', parallel(fonts));
 }
