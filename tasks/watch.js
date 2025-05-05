3bbe18caff6b56302b5ce8a1c6b336218d626470
Tsebom <Tsebom@yandex.ru>
2025-05-05 23:05:13 +0400

    prettier flow

--- tasks/watch.js
+++ tasks/watch.js
@@ -3,6 +3,7 @@ const { watch, parallel, series } = gulp;
 
 import path from 'path';
 import findRemoveSync from 'find-remove';
+import run from 'gulp-run';
 
 import { html } from './html.js';
 import { php } from './php.js';
@@ -17,26 +18,40 @@ import { fonts } from './fonts.js';
 
 export const watching = () => {
 	const wh_ttf = watch('app/fonts/**/*.ttf', parallel(ttf));
-
-	wh_ttf.on('unlink', function (file) {
+	wh_ttf.on('unlink', (file) => {
 		let name = path.basename(file).split('.')[0];
 
 		findRemoveSync('build/fonts/', { prefix: name });
 	});
 
 	const wh_rastr = watch('app/images/**/*.+(png|jpg|jpeg|gif|svg|ico)', parallel(rastr));
-
-	wh_rastr.on('unlink', function (file) {
+	wh_rastr.on('unlink', (file) => {
 		let name = path.basename(file).split('.')[0];
 
 		findRemoveSync('build/images/', { prefix: name });
 		findRemoveSync('app/images/', { prefix: name });
 	});
 
-	watch('app/**/*.html', parallel(html));
-	watch('app/**/*.php', parallel(php));
-	watch('app/**/*.scss', parallel(styles));
-	watch('app/**/*.js', parallel(scripts));
+	const wh_html = watch('app/**/*.html', parallel(html));
+	wh_html.on('change', (file) => {
+		run(`npm run prettier -- ./${file}`).exec();
+	});
+
+	const wh_php = watch('app/**/*.php', parallel(php));
+	wh_php.on('change', (file) => {
+		run(`npm run prettier -- ./${file}`).exec();
+	});
+
+	const wh_style = watch('app/**/*.scss', parallel(styles));
+	wh_style.on('change', (file) => {
+		run(`npm run prettier -- ./${file}`).exec();
+	});
+
+	const wh_script = watch('app/**/*.js', parallel(scripts));
+	wh_script.on('change', (file) => {
+		run(`npm run prettier -- ./${file}`).exec();
+	});
+
 	watch('app/**/*.json', parallel(html));
 	watch('app/svg/sprite/**/*.svg', series(svg_sprite, rastr));
 	watch('build/images/**/*.+(png|jpg|jpeg)', parallel(webp));
